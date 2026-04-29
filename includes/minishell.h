c9ebd6ea5f5a4c17fbab7cd11f23dcb2c41fb668
5BNeumann <flyalalu2@gmail.com>
2026-04-29 03:45:20 +0200

    Modified the header

--- includes/minishell.h
+++ includes/minishell.h
@@ -4,13 +4,19 @@
 # include <stdio.h>
 # include "libft.h"
 
+# define WE 1
+# define RE 0
+
 typedef struct s_command
 {
 	char	*path;
 	char	**arguments;
-	__pid_t	pid;
+	pid_t	pid;
+	char	*infile;
+	char	*outfile;
 }	t_command;
 
 int8_t	execute(t_command cmd, t_ffile out, t_ffile in, char **env);
 char	**command_to_args(char *command);
+void	exec_single(t_command *command, t_ffile stdin, t_ffile stdout);
 #endif
