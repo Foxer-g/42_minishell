8c7ade018027619645449ad31c5748a5b4323b71
f0xer <espinosa.tom7@gmail.com>
2026-05-02 08:43:38 +0200

    update main.c

--- src/main.c
+++ src/main.c
@@ -6,37 +6,11 @@
 /*   By: f0xer <f0xer@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/24 03:09:23 by f0xer             #+#    #+#             */
-/*   Updated: 2026/05/02 04:12:20 by f0xer            ###   ########.fr       */
+/*   Updated: 2026/05/02 08:27:32 by f0xer            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-char	*prompt(void)
-{
-	char	*str_prompt;
-	char	*out;
-	char	*path;
-	char	*usr;
-
-	str_prompt = ft_strdup("\\001");
-	usr = getenv("USER");
-	path = getenv("PWD");
-	if (ft_strlen(path) > (ft_strlen(getenv("HOME"))))
-	{
-		path += ft_strlen(getenv("HOME")) + 5;
-		path[0] = '~';
-	}
-	str_prompt = extend(str_prompt, PROMPT_A);
-	str_prompt = extend(str_prompt, usr);
-	str_prompt = extend(str_prompt, PROMPT_B);
-	str_prompt = extend(str_prompt, path);
-	str_prompt = extend(str_prompt, PROMPT_C);
-	str_prompt = extend(str_prompt, "\\002");
-	out = readline(str_prompt);
-	free(str_prompt);
-	return (out);
-}
 
 int32_t	main(int32_t ac, char **av, char **aenv)
 {
@@ -45,24 +19,27 @@ int32_t	main(int32_t ac, char **av, char **aenv)
 
 	if (ac != 1)
 	{
-		input = "hehehehe";
-//		input = join_tab(++av);
+		input = join_tab(++av);
 //		parsed_input = parser(input);
-//		exec(parsed_input, &aenv);
-		printf("res : %s\n", input);
+//		entry_point(parsed_input, &aenv);
+//		test_print(parsed_input);
+		ft_printf("input : %s\n", input);
+		free(input);
 	}
 	else
 	{
-		input = prompt();
+		input = prompt(aenv);
 		while (input)
 		{
 			add_history(input);
 //			parsed_input = parser(input);
 //			exec(parsed_input, &aenv);
-			printf("res : %s\n", input);
+			ft_printf("input : %s\n", input);
 			free(input);
-			input = prompt(); 
+			input = prompt(aenv); 
 		}
-		return (2);
+		rl_clear_history();
+		ft_printf("exit\n");
+		return (0);
 	}
 }
