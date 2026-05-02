8326bf84defeed0deb8779a9c84a1acaed619a9a
5BNeumann <flyalalu2@gmail.com>
2026-05-02 04:58:57 +0200

    Add tests and change the stupid readline function in GNL to be static

---
+++ tests/exec/test_entrypoint.nim
@@ -0,0 +1,43 @@
+discard """
+  action: "compile"
+  exitcode: 0
+
+  # On Linux 64-bit machines, whether to use Valgrind to check for bad memory
+  # accesses or memory leaks. On other architectures, the test will be run
+  # as-is, without Valgrind.
+  # Options:
+  #   true: run the test with Valgrind
+  #   false: run the without Valgrind
+  #   "leaks": run the test with Valgrind, but do not check for memory leaks
+  valgrind: true
+
+  # Targets to run the test into (c, cpp, objc, js). Defaults to c.
+  targets: "c"
+
+  # Conditions that will skip this test. Use of multiple "disabled" clauses
+  # is permitted.
+  disabled: "win"
+"""
+
+import
+    ../[libft, minishell],
+    std/envvars
+
+let
+  commands: seq[t_command] = @[
+    t_command(
+      path: "/usr/bin/ls",
+      arguments: allocCStringArray(["ls", "-a"])
+    )
+  ]
+  cmdaddrs: ptr t_command = addr commands[0]
+
+var
+  tmp: seq[string] = newSeq[string]()
+  env: cstringArray
+
+for k, v in envPairs():
+  tmp.add(k & "=" & v)
+env = allocCStringArray(tmp)
+
+entrypoint(addr cmdaddrs, env)
---
+++ tests/exec/test_execute.nim
@@ -0,0 +1,48 @@
+discard """
+  action: "compile"
+  exitcode: 0
+
+  outputsub: '''
+testresults
+tests
+'''
+
+  # On Linux 64-bit machines, whether to use Valgrind to check for bad memory
+  # accesses or memory leaks. On other architectures, the test will be run
+  # as-is, without Valgrind.
+  # Options:
+  #   true: run the test with Valgrind
+  #   false: run the without Valgrind
+  #   "leaks": run the test with Valgrind, but do not check for memory leaks
+  valgrind: true
+
+  # Targets to run the test into (c, cpp, objc, js). Defaults to c.
+  targets: "c"
+
+  # Conditions that will skip this test. Use of multiple "disabled" clauses
+  # is permitted.
+  disabled: "win"
+"""
+
+import
+    ../[libft, minishell],
+    std/envvars
+
+let
+  commands: array[1, t_command] = [
+    t_command(
+      path: "/usr/bin/ls",
+      arguments: allocCStringArray(["ls", "-a"])
+    )
+  ]
+
+var
+  tmp: seq[string] = newSeq[string]()
+  env: cstringArray
+
+for k, v in envPairs():
+  tmp.add(k & "=" & v)
+env = allocCStringArray(tmp)
+
+for c in commands:
+  assert(0 == int execute(c, STDOUT_FILENO, STDIN_FILENO, env))
