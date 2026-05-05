75d5fe492eb498286588946302397a2d7575fe52
f0xer <espinosa.tom7@gmail.com>
2026-05-05 05:06:04 +0200

    update main by adding sig gestion

--- src/main.c
+++ src/main.c
@@ -6,16 +6,19 @@
 /*   By: f0xer <f0xer@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/24 03:09:23 by f0xer             #+#    #+#             */
-/*   Updated: 2026/05/02 08:27:32 by f0xer            ###   ########.fr       */
+/*   Updated: 2026/05/04 06:26:49 by f0xer            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+sig_atomic_t	g_sig_handle = 0;
+
 int32_t	main(int32_t ac, char **av, char **aenv)
 {
-	t_command	*parsed_input;
-	char		*input;
+	struct sigaction	sig_action;
+	t_command			*parsed_input;
+	char				*input;
 
 	if (ac != 1)
 	{
@@ -28,15 +31,20 @@ int32_t	main(int32_t ac, char **av, char **aenv)
 	}
 	else
 	{
+		sig_action.sa_sigaction = sig_handle;
+		sig_action.sa_flags = SA_SIGINFO;
+		sigemptyset(&sig_action.sa_mask);
+		sigaction(SIGINT, &sig_action, NULL);
+		sigaction(SIGQUIT, &sig_action, NULL);
 		input = prompt(aenv);
 		while (input)
 		{
 			add_history(input);
 //			parsed_input = parser(input);
-//			exec(parsed_input, &aenv);
+//			entry_point(parsed_input, &aenv);
 			ft_printf("input : %s\n", input);
 			free(input);
-			input = prompt(aenv); 
+			input = prompt(aenv);
 		}
 		rl_clear_history();
 		ft_printf("exit\n");
