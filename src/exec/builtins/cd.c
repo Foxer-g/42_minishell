bef00dac8eda5865895cbe7adda6645a64f9f20c
5BNeumann <flyalalu2@gmail.com>
2026-06-09 04:23:45 +0200

    Add cd and echo (with -n), update the doccube version

---
+++ src/exec/builtins/cd.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                       ⠀⠀⠀⠀⠀⠀⣴⣾⣿⣿⣿⠷⢠⣤⡀      */
+/*   cd.c                                                ⠀⢀⣀⣀⣛⡑⢶⣬⣭⢩⣶⣿⣷⣭⢻⣦⡀    */
+/*                                                       ⣴⠛⢿⡟⠛⢿⣦⠹⣿⡆⣿⣿⣿⣿⣷⢩⡶⠃   */
+/*   By: neumann </var/spool/mail/neumann>               ⣿⣖⠾⢗⣶⣾⣿⡇⠿⠷⠸⠿⢟⣛⡵⣫     */
+/*                                                       ⠙⢿⣿⣿⣿⣿⣿⣿⣮⣭⣭⣭⡭⣶⣾⣿     */
+/*   Created: 2026/06/03 00:51:02 by neumann            ⠀⠀⣿⣿⣿⠛⠛⠛⣿⣿⣿⠁⠀⠀⠉⠁      */
+/*   Updated: 2026/06/03 01:36:01 by neumann            ⠀⠀⠙⠛⠉⠀⠀⠀⠻⠿⠟           */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static bool update_pwd(char ***env)
+{
+	char	pathbuf[PATH_MAX];
+
+	if (getcwd(pathbuf, PATH_MAX))
+	{
+		set_env("PWD", &env, pathbuf);
+		return (true);
+	}
+	perror("cd");
+	return (false);
+}
+
+int	cd(char *path, char **env)
+{
+	if (!path)
+	{
+		perror("Sorry, cd needs at least one argument");
+		return (1);
+	}
+	if (chdir(path))
+	{
+		perror("cd");
+		return (127);
+	}
+	if (!update_pwd(&env))
+		return (1);
+	return (0);
+}
---
+++ src/exec/builtins/echo.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                       ⠀⠀⠀⠀⠀⠀⣴⣾⣿⣿⣿⠷⢠⣤⡀      */
+/*   echo.c                                              ⠀⢀⣀⣀⣛⡑⢶⣬⣭⢩⣶⣿⣷⣭⢻⣦⡀    */
+/*                                                       ⣴⠛⢿⡟⠛⢿⣦⠹⣿⡆⣿⣿⣿⣿⣷⢩⡶⠃   */
+/*   By: neumann </var/spool/mail/neumann>               ⣿⣖⠾⢗⣶⣾⣿⡇⠿⠷⠸⠿⢟⣛⡵⣫     */
+/*                                                       ⠙⢿⣿⣿⣿⣿⣿⣿⣮⣭⣭⣭⡭⣶⣾⣿     */
+/*   Created: 2026/06/03 01:40:00 by neumann            ⠀⠀⣿⣿⣿⠛⠛⠛⣿⣿⣿⠁⠀⠀⠉⠁      */
+/*   Updated: 2026/06/03 02:18:32 by neumann            ⠀⠀⠙⠛⠉⠀⠀⠀⠻⠿⠟           */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	echo(char **argv, t_ffile out)
+{
+	bool	nl;
+
+	argv++;
+	
+	nl = !!ft_strcmp(*argv, "-n");
+	if (!nl)
+		argv++;
+	while (*argv)
+	{
+		ft_putstr_fd(*argv, out);
+		argv++;
+		if (*argv)
+			ft_putchar_fd(' ', out);
+	}
+	if (nl)
+		ft_putchar_fd('\n', out);
+	return (0);
+}
