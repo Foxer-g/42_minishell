0c3d7c707362b530e312c94f186b0ee72408ed02
f0xer <espinosa.tom7@gmail.com>
2026-08-05 02:48:24 +0200

    use brain

--- src/parser/command_gen/command_gen_utils.c
+++ src/parser/command_gen/command_gen_utils.c
@@ -4,13 +4,15 @@
 char	**tkn_to_tab(t_token *tkns, intmax_t i, intmax_t len, char ***env)
 {
 	intmax_t	k;
+	intmax_t	end;
 	char		**res;
 
 	k = 0;
+	end = i + len;
 	res = ft_calloc(len + 1, sizeof(char *));
 	if (!res)
 		return ((void *)((uintptr_t)parsing_error(MALLOC, " : command_gen_utils.c : tkn_to_tab : res", env)));
-	while (i < len)
+	while (i < end)
 	{
 		res[k] = ft_strdup(tkns[i++].content);
 		if (!res[k])
