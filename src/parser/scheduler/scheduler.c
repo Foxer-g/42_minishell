33ed9131a8d69cb82803d91abfd52b4d529a6f52
f0xer <espinosa.tom7@gmail.com>
2026-08-11 04:11:12 +0200

    fix a fixed fix unfixed by a fixed fix

--- src/parser/scheduler/scheduler.c
+++ src/parser/scheduler/scheduler.c
@@ -57,9 +57,8 @@ static bool	command_expand(t_command **cmd, char ***env)
 			return (false);
 		if (!RMV_QT(&(*cmd)[i], true) && !RMV_QT(&(*cmd)[i], false))
 			return (!parsing_error(MALLOC, "", env));
-		if ((*cmd)[i].args && (*cmd)[i].args[0] && (*cmd)[i].args[0][0])
-			if (!cmddup_without_empty(&(*cmd)[i], env))
-				return (false);
+		if (!cmddup_without_empty(&(*cmd)[i], env))
+			return (false);
 	}
 	return (true);
 }
