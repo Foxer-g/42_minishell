53396b1bdcf55fbfe3593a2cab2ea8d142b236db
5BNeumann <flyalalu2@gmail.com>
2026-08-12 05:53:11 +0200

    Norm cmpnd_qt_cntnt

--- src/parser/command_gen/command_gen_utils.c
+++ src/parser/command_gen/command_gen_utils.c
@@ -6,7 +6,7 @@
 /*   By: toespino <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/07 23:49:46 by toespino          #+#    #+#             */
-/*   Updated: 2026/08/11 00:53:30 by neumann            ⠀⠀⠙⠛⠉⠀⠀⠀⠻⠿⠟           */
+/*   Updated: 2026/08/12 05:45:56 by neumann            ⠀⠀⠙⠛⠉⠀⠀⠀⠻⠿⠟           */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,7 +78,29 @@ static bool	gluable(t_token_type type)
 	return (type == COMMAND || type == QUOTE || type == DQUOTE || type == ENV_DESC);
 }
 
-static bool	cmpnd_qt_cntnt(intmax_t i[3], t_token *o, t_token *res, char ***env)
+static char	*resolve_quote_content(intmax_t i[3], t_token *o, t_token_type *res_type)
+{
+	char	*tmp;
+
+	tmp = ft_strdup(o[i[0]].content);
+	if (!tmp)
+		return (NULL);
+	if (o[i[0]].type != DQUOTE && o[i[0]].type != QUOTE)
+	{
+		*res_type = o[i[0]].type;
+		return (tmp);
+	}
+	i[2] = o[i[0]].type;
+	i[0]++;
+	while (((intmax_t) o[i[0]].type) != i[2])
+		tmp = ft_extend(tmp, o[i[0]++].content);
+	if (tmp)
+		tmp = ft_extend(tmp, &o[i[0]].content[0]);
+	*res_type = COMMAND;
+	return (tmp);
+}
+
+static bool	append_quote_token(intmax_t i[3], t_token *o, t_token *res, char ***env)
 {
 	char			*tmp;
 	t_token_type	res_type;
@@ -86,24 +108,9 @@ static bool	cmpnd_qt_cntnt(intmax_t i[3], t_token *o, t_token *res, char ***env)
 
 	glue = (i[1] > 0 && i[0] > 0 && !ends_with_space(o[i[0] - 1].content)
 		&& gluable(o[i[0]].type) && gluable(res[i[1] - 1].type));
-	tmp = ft_strdup(o[i[0]].content);
+	tmp = resolve_quote_content(i, o, &res_type);
 	if (!tmp)
 		return (!parsing_error(MALLOC, "", env));
-	if (o[i[0]].type == DQUOTE || o[i[0]].type == QUOTE)
-	{
-		i[2] = o[i[0]].type;
-		i[0]++;
-		while (((intmax_t) o[i[0]].type) != i[2])
-			tmp = ft_extend(tmp, o[i[0]++].content);
-		if (!tmp)
-			return (!parsing_error(MALLOC, "", env));
-		tmp = ft_extend(tmp, &o[i[0]].content[0]);
-		if (!tmp)
-			return (!parsing_error(MALLOC, "", env));
-		res_type = COMMAND;
-	}
-	else
-		res_type = o[i[0]].type;
 	if (glue)
 	{
 		res[i[1] - 1].content = ft_extend(res[i[1] - 1].content, tmp);
@@ -138,7 +145,7 @@ bool	quote_join(t_token **tkns, char ***env)
 				* sizeof(t_token), i[1] + 2, sizeof(t_token));
 		if (!res)
 			return (!parsing_error(MALLOC, "", env));
-		if (!cmpnd_qt_cntnt(i, *tkns, res, env))
+		if (!append_quote_token(i, *tkns, res, env))
 		{
 			free_token(res);
 			return (false);
